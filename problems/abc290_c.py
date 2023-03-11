193c08ba1897a3c0fed783f7ed0868fba2e1571f
masahiro-999 <masahiro999@outlook.com>
2023-03-11 15:11:23 +0900

    提出 #39591939 wa x 23

---
+++ problems/abc290_c.py
@@ -0,0 +1,15 @@
+N, K = map(int,input().split())
+a = list(map(int,input().split()))
+
+a.sort()
+a = list(set(a))
+
+aa = a[:K]
+
+if aa[0] != 0:
+    print(0)
+else:
+    for i in range(len(aa)):
+        if aa[i] != i:
+            break
+    print(i+1)
