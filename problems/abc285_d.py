3e6f381f245259c4a23fe21a71e56a22dafdfb0a
masahiro-999 <masahiro999@outlook.com>
2023-03-16 21:41:22 +0900

    abc285_d

---
+++ problems/abc285_d.py
@@ -0,0 +1,74 @@
+from collections import defaultdict
+
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
+
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
+
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+
+        if x == y:
+            return True
+
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+        return False
+
+    def size(self, x):
+        return -self.parents[self.find(x)]
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def members(self, x):
+        root = self.find(x)
+        return [i for i in range(self.n) if self.find(i) == root]
+
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x < 0]
+
+    def group_count(self):
+        return len(self.roots())
+
+    def all_group_members(self):
+        group_members = defaultdict(list)
+        for member in range(self.n):
+            group_members[self.find(member)].append(member)
+        return group_members
+
+    def __str__(self):
+        return '\n'.join(f'{r}: {m}' for r, m in self.all_group_members().items())
+
+N, = map(int,input().split())
+
+w = {}
+num = 0
+def w2num(s):
+    global num
+    if w.get(s, None) == None:
+        w[s] = num
+        num += 1
+    return w[s]
+
+u = UnionFind(2*N)
+
+for i in range(N):
+    a,b = input().split()
+    a = w2num(a)
+    b = w2num(b)
+    if u.union(a,b):
+        print ("No")
+        exit()
+print("Yes")
