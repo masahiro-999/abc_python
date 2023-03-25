53c697198819d696c37f5c26e72f73657241174d
masahiro-999 <masahiro999@outlook.com>
2023-03-26 08:55:49 +0900

    abc277_a

---
+++ src/atcoder/abc277/abc277_a.py
@@ -0,0 +1,18 @@
+import sys
+from collections import deque, Counter
+sys.setrecursionlimit(5 * 10 ** 5)
+# from pypyjit import set_param
+# set_param('max_unroll_recursion=-1')
+input = lambda: sys.stdin.readline().rstrip()
+ii = lambda: int(input())
+mi = lambda: map(int, input().split())
+li = lambda: list(mi())
+inf = 2 ** 63 - 1
+mod = 998244353
+
+N,X = mi()
+P = li()
+for i in range(len(P)):
+    if P[i] == X:
+        print(i+1)
+        break
