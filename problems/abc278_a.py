f22e92a05928960f47224ac45106ba322cc8e01d
masahiro-999 <masahiro999@outlook.com>
2023-03-25 19:31:39 +0900

    abc278_a

---
+++ problems/abc278_a.py
@@ -0,0 +1,18 @@
+import sys
+from collections import deque, Counter
+sys.setrecursionlimit(5 * 10 ** 5)
+# from pypyjit import set_param
+# set_param('max_unroll_recursion=-1')
+input = lambda: sys.stdin.readline().rstrip()
+ii = lambda: int(input())
+mi = lambda: map(int, input().split())
+li = lambda: list(mi())
+inf = 2 ** 63 - 1
+mod = 998244353
+
+N,K  = li()
+
+a = li()
+
+ans = (a+([0]*K))[K:]
+print(*ans)
