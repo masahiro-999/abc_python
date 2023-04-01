be9c21111f7ab42a8b4b0d244e85288be53031dc
Masahiro Tsuji <masa.tsuji@gmail.com>
2023-04-01 20:16:44 +0900

    abc148_e

--- src/atcoder/abc148/abc148_e.py
+++ src/atcoder/abc148/abc148_e.py
@@ -1,6 +1,14 @@
 n, = map(int, input().split())
 
 if n%2 == 0:
-    print(n//10)
+    ans = 0
+    i = 1
+    while True:
+        if n>=5**i*2:
+            ans += n//(5**i*2)
+            i += 1
+        else:
+            break
+    print(ans)
 else:
     print("0")
